8cd68a3b0bea9f97f73258b870e3d0eebc7b7eb4
ttruongnee <dinhthientruong2192004@gmail.com>
2025-12-09 11:41:47 +0700

    create models

---
+++ AccessControlAPI/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿namespace AccessControlAPI.Models
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Role() { }
+        public Role(string id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
---
+++ AccessControlAPI/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace AccessControlAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public User() { }
+        public User(int id, string username, string password)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+        }
+    }
+}
