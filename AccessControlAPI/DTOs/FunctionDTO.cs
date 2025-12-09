293393fbdb272945572c51601a8b4e30937ad717
ttruongnee <dinhthientruong2192004@gmail.com>
2025-12-09 11:45:57 +0700

    create dtos

---
+++ AccessControlAPI/DTOs/FunctionDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace AccessControlAPI.DTOs
+{
+    public class FunctionDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Sort_order { get; set; }
+        public int? Parent_id { get; set; }
+        public bool Show_search { get; set; }
+        public bool Show_add { get; set; }
+        public bool Show_update { get; set; }
+        public bool Show_delete { get; set; }
+    }
+}
---
+++ AccessControlAPI/DTOs/UserDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace AccessControlAPI.DTOs
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
