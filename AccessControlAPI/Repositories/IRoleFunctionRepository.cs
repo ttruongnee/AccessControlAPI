2852853c3db3515c467a4a1dc1cbc0f73db5dc0d
ttruongnee <dinhthientruong2192004@gmail.com>
2025-12-10 14:37:04 +0700

    create RoleFunction

---
+++ AccessControlAPI/Repositories/IRoleFunctionRepository.cs
@@ -0,0 +1,11 @@
+﻿using AccessControlAPI.Models;
+
+namespace AccessControlAPI.Repositories
+{
+    public interface IRoleFunctionRepository
+    {
+        List<Function> GetFunctionIdsByRoleId(int roleId);
+        bool AddFunctionsForRole(int roleId, List<string> functionIds);
+        bool DeleteFunctionsFromRole(int roleId);
+    }
+}
---
+++ AccessControlAPI/Repositories/RoleFunctionRepository.cs
@@ -0,0 +1,58 @@
+﻿
+using AccessControlAPI.Database;
+using AccessControlAPI.Models;
+using Dapper;
+
+namespace AccessControlAPI.Repositories
+{
+    public class RoleFunctionRepository : IRoleFunctionRepository
+    {
+        private readonly IOracleDb _oracleDb;
+        public RoleFunctionRepository(IOracleDb oracleDb)
+        {
+            _oracleDb = oracleDb;
+        }
+        public bool AddFunctionsForRole(int roleId, List<string> functionIds)
+        {
+            using (var conn = _oracleDb.GetConnection())
+            {
+                try
+                {
+                    string sql = "insert into roles_functions (role_id, function_id) values (:roleId, :functionId)";
+                    foreach (var functionId in functionIds)
+                    {
+                        conn.Execute(sql, new { roleId, functionId });
+                    }
+                    return true;
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool DeleteFunctionsFromRole(int roleId)
+        {
+            using (var conn = _oracleDb.GetConnection())
+            {
+                string sql = "delete from roles_functions where role_id = :roleId";
+                return conn.Execute(sql, new { roleId }) > 0;
+            }
+        }
+
+        public List<Function> GetFunctionIdsByRoleId(int roleId)
+        {
+            using (var conn = _oracleDb.GetConnection())
+            {
+                string sql = @"
+                                SELECT f.*
+                                FROM functions f
+                                INNER JOIN roles_functions rf ON f.id = rf.function_id
+                                WHERE rf.role_id = :roleId";
+
+                return conn.Query<Function>(sql, new { roleId }).ToList();
+            }
+        }
+    }
+}
