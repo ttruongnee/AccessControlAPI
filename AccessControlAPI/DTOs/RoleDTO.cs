0199c6ac098685d46c800b1b566030ece9408cf0
ttruongnee <dinhthientruong2192004@gmail.com>
2025-12-15 15:31:43 +0700

    delete reponsitory refresh token

--- AccessControlAPI/DTOs/RoleDTO.cs
+++ AccessControlAPI/DTOs/RoleDTO.cs
@@ -1,12 +1,17 @@
-﻿namespace AccessControlAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccessControlAPI.DTOs
 {
     public class RoleDTO
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
     public class CreateUpdateRoleDTO
     {
+        [Required]
         public string Name { get; set; }
     }
 }
--- AccessControlAPI/DTOs/UserDTO.cs
+++ AccessControlAPI/DTOs/UserDTO.cs
@@ -1,14 +1,20 @@
-﻿namespace AccessControlAPI.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AccessControlAPI.DTOs
 {
     public class UserNoPasswordDTO
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Username { get; set; }
     }
 
     public class UserDTO
     {
+        [Required]
         public string Username { get; set; }
+        [Required]
         public string Password { get; set; }
     }
 }
